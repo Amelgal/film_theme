b4a3ffd1d0b88ad6453abab98eb34206bf0a288f
Eugene Shalko <eugene.shalko.00@gmail.com>
2021-01-07 22:27:31 +0200

    Issue#7
    -change metabox view

--- wordpress/wp-content/themes/film_theme/class-metabox/class-poster-path-metabox.php
+++ wordpress/wp-content/themes/film_theme/class-metabox/class-poster-path-metabox.php
@@ -15,15 +15,6 @@ class Poster_Metabox extends Base_Meta
     $poster_path = get_post_meta($post->ID, 'poster_path', true);
 
     ?>
-    <p>
-      <label for="poster_path-select"><?php _e('Select poster_path of type') ?></label>
-      <select name="poster_path" id="poster_path-select">
-        <option value="high">       <?php _e('High') ?>     </option>
-        <option value="average">    <?php _e('Average') ?>  </option>
-        <option value="low">        <?php _e('Low') ?>      </option>
-      </select>
-    </p>
-    <hr>
     <label for="poster_path"><?php _e('poster_path') ?></label>
     <input type="text" name="poster_path" id="poster_path" value="<?= ucfirst($poster_path); ?>" disabled>
     <?php
